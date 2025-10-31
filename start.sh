6dc29e22fb1cb38a0ff887527e5288806d24a2d7
MatheusRecieri <matheusrecieri@gmail.com>
2025-10-31 13:34:47 -0300

    Correçao 10

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 pip install --upgrade pip setuptools wheel
 
 npm run start &
-uvicorn app.main:app --host 0.0.0.0 --port 5000 --reload
+cd python-service && uvicorn app.main:app --host 0.0.0.0 --port 5000 --reload
