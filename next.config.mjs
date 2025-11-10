aa9d0c0ac7f0fabce916e3b31a799fd195113b76
MatheusRecieri <matheusrecieri@gmail.com>
2025-11-10 15:18:50 -0300

    Corrigindo configurações do next ´para docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,25 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   devIndicators: false,
+
+//   output: 'standalone',
+
+//   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+
+//   env: {
+//     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+//   },
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
   devIndicators: false,
 
-  output: 'standalone',
+}
 
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
-  },
-};
-
-export default nextConfig;
+module.exports = nextConfig
