a34958efb81f106fdf1db9d7a70c151cc9bb9ace
MatheusRecieri <matheusrecieri@gmail.com>
2025-11-11 08:59:37 -0300

    Corrigindo dockerfile e next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  devIndicators: false,
-
-  output: 'standalone',
-
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
-  },
-};
-
-export default nextConfig;
-
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   reactStrictMode: true,
+//   /* config options here */
 //   devIndicators: false,
-//   output: 'standalone'
 
-// }
+//   output: 'standalone',
 
-// export default nextConfig;  
+//   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+
+//   env: {
+//     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+//   },
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  devIndicators: false,
+  // output: 'standalone'
+
+}
+
+export default nextConfig;  
