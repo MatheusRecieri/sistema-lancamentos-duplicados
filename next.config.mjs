601960eec320c6edeb9ebc06968451529cb6f716
MatheusRecieri <matheusrecieri@gmail.com>
2025-11-11 10:19:34 -0300

    Mudando o next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,25 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   /* config options here */
-//   devIndicators: false,
-
-//   output: 'standalone',
-
-//   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-
-//   env: {
-//     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
-//   },
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  /* config options here */
   devIndicators: false,
 
-}
+  output: 'standalone',
 
-export default nextConfig;  
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+  },
+};
+
+export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   devIndicators: false,
+
+// }
+
+// export default nextConfig;  
