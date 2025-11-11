fa821a820184416ce021d6ddf381d4b27f2f87f5
MatheusRecieri <matheusrecieri@gmail.com>
2025-11-11 14:38:45 -0300

    Alteração 17, corrigindo o server.js brackend

--- backend/server.js
+++ backend/server.js
@@ -10,38 +10,41 @@ dotenv.config();
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const PORT = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'development';
-// const app = next({ dev, dir: './frontend' });
-// const handle = app.getRequestHandler();
+const app = next({ dev, dir: './frontend' });
+const handle = app.getRequestHandler();
 
 
-const server = express();
+app.prepare().then(() => {
 
-// console.log(FRONTEND_URL);
+  const server = express();
 
-server.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+  // console.log(FRONTEND_URL);
 
-server.use(express.json());
+  server.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }));
 
-// Middleware de log ANTES das rotas
-server.use((req, res, next) => {
-  // console.log(`📨 ${req.method} ${req.path}`);
-  next();
+  server.use(express.json());
+
+  // Middleware de log ANTES das rotas
+  server.use((req, res, next) => {
+    // console.log(`📨 ${req.method} ${req.path}`);
+    next();
+  });
+
+  // CORREÇÃO: era app.search, agora é app.use
+  server.use("/uploads", express.static(path.resolve("uploads")));
+
+  server.use("/files", fileRoutes); //antiga api
+
+  server.all('/{*splat}', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`🚀 Servidor Next.js/Express rodando em ${FRONTEND_URL}`);
+  });
+
 });
-
-// CORREÇÃO: era app.search, agora é app.use
-server.use("/uploads", express.static(path.resolve("uploads")));
-
-server.use("/files", fileRoutes); //antiga api
-
-server.all('/{*splat}', (req, res) => {
-  return handle(req, res);
-});
-
-server.listen(PORT, (err) => {
-  if (err) throw err;
-  console.log(`🚀 Servidor Next.js/Express rodando em ${FRONTEND_URL}`);
-});
-
