040f852535f4d5f147c98fc70b7e73201a81c23e
MatheusRecieri <matheusrecieri@gmail.com>
2025-11-12 09:29:28 -0300

    Alteração 30, voltando commit anterior

--- python-service/app/main.py
+++ python-service/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
-
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import tempfile
 import os
@@ -18,13 +17,13 @@ app = FastAPI(
 )
 
 # CORS para integração com node.js
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:3000", "http://localhost:4000"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:4000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 pdf_reader = PDFReader()
 analyzer = DuplicateAnalyzer()
