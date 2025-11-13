3dff2f34b06ec48f1c16106dc254301614503723
MatheusRecieri <matheusrecieri@gmail.com>
2025-11-13 16:30:55 -0300

    Testes di regex

--- python-service/app/services/pdf_reader.py
+++ python-service/app/services/pdf_reader.py
@@ -95,7 +95,8 @@ class PDFReader:
         patterns = [
             # Padrão completo: CÓDIGO DATA NOTA FORNECEDOR VALOR_CONTABIL VALOR
             # |Código|| Espaços||        Data       ||espaços||nf |         |forn|   |valorcot| |valor|
-            r"(\d{3,6})\s+(\d{2}/\d{2}/\d{2,4})\s+(\d+)\s+\d+\s+\d+\s+([A-Z][\w\s&\-\.]+?)\s+\d-\d{3}\s+\d+\s+[A-Z]{2}\s+([\d.,]+)",
+            # r"(\d{3,6})\s+(\d{2}/\d{2}/\d{2,4})\s+(\d+)\s+\d+\s+\d+\s+([A-Z][\w\s&\-\.]+?)\s+\d-\d{3}\s+\d+\s+[A-Z]{2}\s+([\d.,]+)",
+            r"^\d{3,6}\s+\d{2}/\d{2}/\d{4}\s+(\d{2}/\d{2}/\d{4})\s+(\d+)\s+\d+\s+\d+\s*-\s*([A-Z0-9\s\.\-]+?)\s+\d-\d+[A-Z]{2}\s+([\d.,]+)",
             # Padrão sem código: DATA NOTA FORNECEDOR VALOR
             r"(\d{2}/\d{2}/\d{2,4})\s+(\d+)\s+(.{10,}?)\s+([\d.,]+)\s+([\d.,])",
             # Padrão minimalista: FORNECEDOR DATA VALOR
